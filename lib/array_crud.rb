adcbdaec64247e87ddd0d909b0318e548dc8bb6c
Joanna E K <kalemajoanna@gmail.com>
2019-10-29 17:51:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -53,7 +53,6 @@ end
 def update_element_from_index(array, index_number, element)
  array = ["wow", "I", "am", "really", "learning", "arrays!"]
  index_number = 4
- element = "totally"
  array [4] = "totally"
  p array
 end
