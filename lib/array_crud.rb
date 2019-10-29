3490163e3ab1512f4b24149e431a71c928725e88
Joanna E K <kalemajoanna@gmail.com>
2019-10-29 17:00:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,8 +25,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.pop
-  p array
+  p array.pop
 end
 
 def remove_element_from_start_of_array(array)
