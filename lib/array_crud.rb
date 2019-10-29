96d6b6f299bf42b85fb9c1fdd0a2fa0139055762
Joanna E K <kalemajoanna@gmail.com>
2019-10-29 17:10:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -42,7 +42,7 @@ def retrieve_element_from_index(array, index_number)
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  array
   
   
 end
